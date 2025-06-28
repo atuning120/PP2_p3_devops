58cf0ff3d9d55ab11d836c8968a6919da4e4fff9
atuning120 <90119017+atuning120@users.noreply.github.com>
2025-06-27 20:50:34 -0400

    cambios

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <h1>Prueba 2 parte 3 B DevOps</h1>
+    <div>
+      <h1>Prueba 2 parte 3 B DevOps</h1>
+      <h1>Segunda linea a modo de cambios</h1>
+    </div>
   );
 }
 
